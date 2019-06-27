fada415eef81491b4b21404a4043962f760f0703
Renan Kaic Lopes <renan-kaic@outlook.com>
2019-06-27 16:43:47 -0300

    First steps with TDD - Creating a calculator from TDD

---
+++ src/main/java/br/ce/wcaquino/servicos/Calculadora.java
@@ -0,0 +1,25 @@
+package br.ce.wcaquino.servicos;
+
+import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
+
+public class Calculadora {
+
+	public int somar(int a, int b) {
+		// TODO Auto-generated method stub
+		return a + b;
+	}
+
+	public int subtracao(int a, int b) {
+		// TODO Auto-generated method stub
+		return a - b;
+	}
+
+	public int divide(int a, int b) throws NaoPodeDividirPorZeroException {
+		// TODO Auto-generated method stub
+		if ( b == 0 ) {
+			throw new NaoPodeDividirPorZeroException();
+		}
+		return a / b;
+	}
+
+}
