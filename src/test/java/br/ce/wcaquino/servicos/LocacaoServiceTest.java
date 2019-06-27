4a2bea1dd67ced29680cff281f32a7b30190c334
Renan Kaic Lopes <renan-kaic@outlook.com>
2019-06-27 14:06:55 -0300

    Adding 3 exception handling forms

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -7,9 +7,11 @@ import static org.hamcrest.CoreMatchers.is;
 
 import java.util.Date;
 
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -21,24 +23,79 @@ public class LocacaoServiceTest {
 	@Rule
 	public ErrorCollector error = new ErrorCollector();
 	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+	
 	@Test
-	public void testeLocacao() {
+	public void testeLocacao() throws Exception {
 		
 		//cenário
 		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Renan");
-		Filme filme = new Filme("FIlme 1", 2, 5.0);
+		Filme filme = new Filme("Filme 1", 2, 5.0);
 		
 		//ação
-		Locacao locacao = service.alugarFilme(usuario, filme);
-		
+		Locacao locacao;
+		locacao = service.alugarFilme(usuario, filme);
+			
 		//verificação
-		//Assert.assertEquals(5.0, locacao.getValor(),  0.01);
 		error.checkThat(locacao.getValor(), is(equalTo(5.0)));
-		//assertThat(locacao.getValor(), is(not(6.0)));
 		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
 		error.checkThat(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+			
+	}
+	
+	//Forma Elegante - Espera que realmente dê uma exceção
+	//Se não der exceção, o teste falha
+	@Test(expected = Exception.class)
+	public void testeLocacao_filmeSemEstoque1() throws Exception{
 		
+		//cenário
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Renan");
+		Filme filme = new Filme("Filme 1", 0, 5.0);
+		
+		//ação
+		service.alugarFilme(usuario, filme);		
+	
+	}
+	
+	//Forma robusta - fornece um controle maior sobre o teste
+	//Espera que lance uma exceção para o teste funcionar
+	@Test
+	public void testeLocacao_filmeSemEstoque2(){
+		
+		//cenário
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Renan");
+		Filme filme = new Filme("Filme 1", 0, 5.0);
+		
+		//ação
+		try {
+			service.alugarFilme(usuario, filme);
+			Assert.fail("Deveria ter lançado uma exceção.");
+		} catch (Exception e) {
+			Assert.assertThat(e.getMessage(), is("Filme sem estoque"));
+		}		
+	
+	}
+	
+	//Forma nova 
+	@Test
+	public void testeLocacao_filmeSemEstoque3() throws Exception{
+		
+		//cenário
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Renan");
+		Filme filme = new Filme("Filme 1", 0, 5.0);
+		
+		//espera pela exceção
+		exception.expect(Exception.class);
+		exception.expectMessage("Filme sem estoque");
+		
+		//ação
+		service.alugarFilme(usuario, filme);			
+	
 	}
 	
 }
